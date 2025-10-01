d809e6c3c61de5020395e0db7b67c97e5a02e35c
José Manuel Alvarez <97788933+profjosemanuel@users.noreply.github.com>
2025-10-01 14:53:31 +0200

    Create calculadora_simple_amb_enters_sense_decimals.c

---
+++ calculadora_simple_amb_enters_sense_decimals.c
@@ -0,0 +1,39 @@
+/**
+ * @brief Implements a basic four-function calculator.
+ *
+ * This program prompts the user to enter two numbers (operands)
+ * and an arithmetic operator (+, -, *, /). It then performs the
+ * calculation and prints the result, handling division by zero errors.
+ */
+int main() {
+    int operator;
+    int operand1, operand2, result;
+    
+    operator = 3;
+    operand1 = 10;
+    operand2 = 2;
+
+    if (operator==0){
+        return operand1 + operand2;
+
+    }
+
+    if (operator==1){
+        return operand1 - operand2;
+
+    }   
+
+
+    if (operator==2){
+        return operand1 * operand2;
+
+    }   
+
+     if (operator==3){
+        return operand1 / operand2;
+
+    }    
+  
+ 
+    return 0;
+}
